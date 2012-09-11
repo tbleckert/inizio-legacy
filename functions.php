a781703e0c8e4c47ef7b91e595680066eb095aca
tbleckert <tbleckert@gmail.com>
2012-09-11 10:33:04 +0200

    Reorganized

--- functions.php
+++ functions.php
@@ -23,14 +23,14 @@ sidebars, comments, ect.
 	- custom google+ integration
 	- adding custom fields to user profiles
 */
-require_once('library/bones.php'); // if you remove this, bones will break
+require_once('inc/bones.php'); // if you remove this, bones will break
 /*
 2. library/custom-post-type.php
     - an example custom post type
     - example custom taxonomy (like categories)
     - example custom taxonomy (like tags)
 */
-require_once('library/custom-post-type.php'); // you can disable this if you like
+require_once('inc/custom-post-type.php'); // you can disable this if you like
 /*
 3. library/admin.php
     - removing some default WordPress dashboard widgets
