859699b6b33a8d72a4dc0d96c8f0bb6ba23ff3bb
tbleckert <tbleckert@gmail.com>
2012-09-12 08:47:44 +0200

    Modified the custom post type example and added a post type helper to simplify things

--- functions.php
+++ functions.php
@@ -1,29 +1,19 @@
 <?php
 /*
-Author: Eddie Machado
+Author: Tobias Bleckert
+URL: htp://tb-one.se
+
+Original author: Eddie Machado
 URL: htp://themble.com/bones/
 
-This is where you can drop your custom functions or
-just edit things like thumbnail sizes, header images, 
-sidebars, comments, ect.
+This theme was forked from Eddie Machado (https://github.com/eddiemachado/bones) 
+and now updated to fit my needs and hopefully yours.
 */
 
-/************* INCLUDE NEEDED FILES ***************/
+/* Include files */
+require_once('inc/bones.php'); // required
+require_once('inc/custom-post-type.php'); // optional
 
-/*
-1. library/bones.php
-    - head cleanup (remove rsd, uri links, junk css, ect)
-	- enqueueing scripts & styles
-	- theme support functions
-    - custom menu output & fallbacks
-	- related post function
-	- page-navi function
-	- removing <p> from around images
-	- customizing the post excerpt
-	- custom google+ integration
-	- adding custom fields to user profiles
-*/
-require_once('inc/bones.php'); // if you remove this, bones will break
 Bones::ahoy();
 
 $assets = array(
@@ -58,51 +48,9 @@ $assets = array(
 
 Bones::assets($assets);
 
-/*
-2. library/custom-post-type.php
-    - an example custom post type
-    - example custom taxonomy (like categories)
-    - example custom taxonomy (like tags)
-*/
-require_once('inc/custom-post-type.php'); // you can disable this if you like
-/*
-3. library/admin.php
-    - removing some default WordPress dashboard widgets
-    - an example custom dashboard widget
-    - adding custom login css
-    - changing text in footer of admin
-*/
-// require_once('library/admin.php'); // this comes turned off by default
-/*
-4. library/translation/translation.php
-    - adding support for other languages
-*/
-// require_once('library/translation/translation.php'); // this comes turned off by default
-
-/************* THUMBNAIL SIZE OPTIONS *************/
-
 // Thumbnail sizes
 add_image_size( 'bones-thumb-600', 600, 150, true );
 add_image_size( 'bones-thumb-300', 300, 100, true );
-/* 
-to add more sizes, simply copy a line from above 
-and change the dimensions & name. As long as you
-upload a "featured image" as large as the biggest
-set width or height, all the other sizes will be
-auto-cropped.
-
-To call a different size, simply change the text
-inside the thumbnail function.
-
-For example, to call the 300 x 300 sized image, 
-we would use the function:
-<?php the_post_thumbnail( 'bones-thumb-300' ); ?>
-for the 600 x 100 image:
-<?php the_post_thumbnail( 'bones-thumb-600' ); ?>
-
-You can change the names and dimensions to whatever
-you like. Enjoy!
-*/
 
 
 ?>
