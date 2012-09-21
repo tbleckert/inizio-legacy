482e4f39ee241e79c067ba413c7409e5b567de3f
tbleckert <tbleckert@gmail.com>
2012-09-21 16:28:14 +0200

    CPT icon

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -24,28 +24,27 @@ function register_custom_post_example() {
 	new Custom_Post_Type_Helper('article',
 		array(
 			'labels'               => array(
-				'name'               => __('Articles', 'bonestheme'), /* This is the Title of the Group */
-				'singular_name'      => __('Article', 'bonestheme'), /* This is the individual type */
-				'all_items'          => __('All articles', 'bonestheme'), /* the all items menu item */
-				'add_new'            => __('Add New', 'bonestheme'), /* The add new menu item */
-				'add_new_item'       => __('Add New Article', 'bonestheme'), /* Add New Display Title */
-				'edit'               => __( 'Edit', 'bonestheme' ), /* Edit Dialog */
-				'edit_item'          => __('Edit Articles', 'bonestheme'), /* Edit Display Title */
-				'new_item'           => __('New Article', 'bonestheme'), /* New Display Title */
-				'view_item'          => __('View Article', 'bonestheme'), /* View Display Title */
-				'search_items'       => __('Search Article', 'bonestheme'), /* Search Custom Type Title */ 
-				'not_found'          =>  __('Nothing found in the Database.', 'bonestheme'), /* This displays if there are no entries yet */ 
-				'not_found_in_trash' => __('Nothing found in Trash', 'bonestheme'), /* This displays if there is nothing in the trash */
+				'name'               => __('Articles', 'inizio'), /* This is the Title of the Group */
+				'singular_name'      => __('Article', 'inizio'), /* This is the individual type */
+				'all_items'          => __('All articles', 'inizio'), /* the all items menu item */
+				'add_new'            => __('Add New', 'inizio'), /* The add new menu item */
+				'add_new_item'       => __('Add New Article', 'inizio'), /* Add New Display Title */
+				'edit'               => __( 'Edit', 'inizio' ), /* Edit Dialog */
+				'edit_item'          => __('Edit Articles', 'inizio'), /* Edit Display Title */
+				'new_item'           => __('New Article', 'inizio'), /* New Display Title */
+				'view_item'          => __('View Article', 'inizio'), /* View Display Title */
+				'search_items'       => __('Search Article', 'inizio'), /* Search Custom Type Title */ 
+				'not_found'          =>  __('Nothing found in the Database.', 'inizio'), /* This displays if there are no entries yet */ 
+				'not_found_in_trash' => __('Nothing found in Trash', 'inizio'), /* This displays if there is nothing in the trash */
 				'parent_item_colon'  => ''
 			),
-			'description'          => __( 'This is the example custom post type', 'bonestheme' ), /* Custom Type Description */
+			'description'          => __( 'This is the example custom post type', 'inizio' ), /* Custom Type Description */
 			'public'               => true,
 			'publicly_queryable'   => true,
 			'exclude_from_search'  => false,
 			'show_ui'              => true,
 			'query_var'            => true,
 			'menu_position'        => 8, /* this is what order you want it to appear in on the left hand side menu */ 
-			'menu_icon'            => get_stylesheet_directory_uri() . '/assets/img/custom-post-icon.png', /* the icon for the custom post type menu */
 			'rewrite'	             => false, /* you can specify its url slug */
 			'has_archive'          => 'articles', /* you can rename the slug here */
 			'capability_type'      => 'post',
@@ -65,16 +64,16 @@ function register_custom_post_example() {
 		array('article'), /* if you change the name of register_post_type( 'custom_type', then you have to change this */
 		array('hierarchical' => true,     /* if this is true, it acts like categories */             
 			'labels' => array(
-			'name' => __( 'Custom Categories', 'bonestheme' ), /* name of the custom taxonomy */
-			'singular_name' => __( 'Custom Category', 'bonestheme' ), /* single taxonomy name */
-			'search_items' =>  __( 'Search Custom Categories', 'bonestheme' ), /* search title for taxomony */
-			'all_items' => __( 'All Custom Categories', 'bonestheme' ), /* all title for taxonomies */
-			'parent_item' => __( 'Parent Custom Category', 'bonestheme' ), /* parent title for taxonomy */
-			'parent_item_colon' => __( 'Parent Custom Category:', 'bonestheme' ), /* parent taxonomy title */
-			'edit_item' => __( 'Edit Custom Category', 'bonestheme' ), /* edit custom taxonomy title */
-			'update_item' => __( 'Update Custom Category', 'bonestheme' ), /* update title for taxonomy */
-			'add_new_item' => __( 'Add New Custom Category', 'bonestheme' ), /* add new title for taxonomy */
-			'new_item_name' => __( 'New Custom Category Name', 'bonestheme' ) /* name title for taxonomy */
+			'name' => __( 'Custom Categories', 'inizio' ), /* name of the custom taxonomy */
+			'singular_name' => __( 'Custom Category', 'inizio' ), /* single taxonomy name */
+			'search_items' =>  __( 'Search Custom Categories', 'inizio' ), /* search title for taxomony */
+			'all_items' => __( 'All Custom Categories', 'inizio' ), /* all title for taxonomies */
+			'parent_item' => __( 'Parent Custom Category', 'inizio' ), /* parent title for taxonomy */
+			'parent_item_colon' => __( 'Parent Custom Category:', 'inizio' ), /* parent taxonomy title */
+			'edit_item' => __( 'Edit Custom Category', 'inizio' ), /* edit custom taxonomy title */
+			'update_item' => __( 'Update Custom Category', 'inizio' ), /* update title for taxonomy */
+			'add_new_item' => __( 'Add New Custom Category', 'inizio' ), /* add new title for taxonomy */
+			'new_item_name' => __( 'New Custom Category Name', 'inizio' ) /* name title for taxonomy */
 		),
 			'show_ui' => true,
 			'query_var' => true,
@@ -87,16 +86,16 @@ function register_custom_post_example() {
 		array('article'),
 		array('hierarchical' => false,               
 			'labels' => array(
-			'name' => __( 'Custom Tags', 'bonestheme' ),
-			'singular_name' => __( 'Custom Tag', 'bonestheme' ),
-			'search_items' =>  __( 'Search Custom Tags', 'bonestheme' ),
-			'all_items' => __( 'All Custom Tags', 'bonestheme' ),
-			'parent_item' => __( 'Parent Custom Tag', 'bonestheme' ),
-			'parent_item_colon' => __( 'Parent Custom Tag:', 'bonestheme' ),
-			'edit_item' => __( 'Edit Custom Tag', 'bonestheme' ),
-			'update_item' => __( 'Update Custom Tag', 'bonestheme' ),
-			'add_new_item' => __( 'Add New Custom Tag', 'bonestheme' ),
-			'new_item_name' => __( 'New Custom Tag Name', 'bonestheme' )
+			'name' => __( 'Custom Tags', 'inizio' ),
+			'singular_name' => __( 'Custom Tag', 'inizio' ),
+			'search_items' =>  __( 'Search Custom Tags', 'inizio' ),
+			'all_items' => __( 'All Custom Tags', 'inizio' ),
+			'parent_item' => __( 'Parent Custom Tag', 'inizio' ),
+			'parent_item_colon' => __( 'Parent Custom Tag:', 'inizio' ),
+			'edit_item' => __( 'Edit Custom Tag', 'inizio' ),
+			'update_item' => __( 'Update Custom Tag', 'inizio' ),
+			'add_new_item' => __( 'Add New Custom Tag', 'inizio' ),
+			'new_item_name' => __( 'New Custom Tag Name', 'inizio' )
 		),
 			'show_ui' => true,
 			'query_var' => true,
@@ -107,4 +106,11 @@ function register_custom_post_example() {
 // adding the function to the Wordpress init
 add_action( 'init', 'register_custom_post_example');
 
-?>
+// Setting icon
+add_action( 'admin_head', 'cpt_icons' );
+function cpt_icons() { ?>
+  <style type="text/css" media="screen">
+      #menu-posts-article .wp-menu-image { background: url(<?php echo get_stylesheet_directory_uri() . '/assets/img/icons/pencil.png' ?>) no-repeat 6px -17px !important; }
+      #menu-posts-article:hover .wp-menu-image, #menu-posts-article.wp-has-current-submenu .wp-menu-image { background-position:6px 7px!important; }
+  </style>
+<?php } ?>
