bde86fb0497c9eb5d582921f367a85fc83207915
tbleckert <tbleckert@gmail.com>
2012-09-26 16:52:26 +0200

    Project updates

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<meta charset="utf-8">
+	
+		<title><?php bloginfo('name'); ?> <?php wp_title( '' ); ?></title>
+		
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+		<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" />
+		
+		<?php wp_head(); ?>
+	</head>
+	
+	<body>
