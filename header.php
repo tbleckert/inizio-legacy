1f5b5b1911071a9675c5694e4d0e635fa0e1cf6a
Tobias Bleckert <tobias@vinnovera.se>
2013-05-12 17:52:09 +0200

    Recommend using Wordpress SEO by Yoast to display the title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 	
-		<title><?php bloginfo('name'); ?> <?php wp_title( '' ); ?></title>
+		<title><?php wp_title( '' ); ?></title>
 		
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 		<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" />
